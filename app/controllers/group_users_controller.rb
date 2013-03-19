2b70b0d614b46a551676f26ab50d338d98d40685
Brandon Gafford <brandon.gafford@gmail.com>
2013-03-19 13:40:10 -0400

    fixed user-group association, list users on group page, list groups on user page, change text messages to send to a group

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -12,7 +12,8 @@ class GroupUsersController < ApplicationController
   end
 
   def create
-    @group_user = GroupUser.new(group_id: Group.find_by_name(params[:group_name]).id, user_id: User.find_by_name(params[:user_name]).id)
+    @group_user = GroupUser.new(group_id: Group.find_by_name(params[:group_name]).id,
+        user_id: User.find_by_name(params[:user_name]).id)
     if @group_user.save
       flash[:success] = "New association created!"
       redirect_to @group_user
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ActiveRecord::Base
   attr_accessible :name
+  has_many :group_users
   has_many :users, :through => :group_users
 
   validates :name, presence: true, uniqueness: true,
