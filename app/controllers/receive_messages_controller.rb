d39103458b65b0088ec82fa3344002a2680fab82
Garrett Lord <garrettlord@gmail.com>
2013-03-26 22:26:47 -0400

    fixed the gemfile v2

--- app/controllers/receive_messages_controller.rb
+++ app/controllers/receive_messages_controller.rb
@@ -4,8 +4,12 @@ class ReceiveMessagesController < ApplicationController
   def process_sms
     @city = params[:FromCity].capitalize
     @state = params[:FromState]
-    @body = params[:Body]
-    @fromNum = params[:From]
+    body = params[:Body]
+    words = body.split(" ")
+    @group = words[0]
+    @message = words[1..-1]
+    fromNum = params[:From]
+    @userName = User.find_by_phone_number(fromNum).name ||= "UNKNOWN"
     render '/list_texts/list_texts.xml.erb', :content_type => 'text/xml'
   end
 
