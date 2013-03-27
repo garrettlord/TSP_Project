5872f905f58e7eda7ffa85c30b3c54842c28c4cc
Garrett Lord <garrettlord@gmail.com>
2013-03-26 22:12:27 -0400

    fixed the gemfile v2

--- app/controllers/receive_messages_controller.rb
+++ app/controllers/receive_messages_controller.rb
@@ -4,6 +4,8 @@ class ReceiveMessagesController < ApplicationController
   def process_sms
     @city = params[:FromCity].capitalize
     @state = params[:FromState]
+    @body = params[:Body]
+    @fromNum = params[:From]
     render '/list_texts/list_texts.xml.erb', :content_type => 'text/xml'
   end
 
