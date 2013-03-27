34d669096bc75f0da925c1d7f8e43a06e70d1d83
krerkkil <krerkkil@mtu.edu>
2013-03-27 16:01:39 -0400

    BASIC API

--- api_scripts/weather.rb
+++ api_scripts/weather.rb
@@ -1,20 +1,55 @@
 require 'rubygems'
 require 'httparty'
 require 'ywx'
-require 'pp'
-appid = 'LCQO6F3V34FE6FIki6XMq3Pg8pd46EgovEi7AYFFTcA4cStxIre2CB8tpE7Xy.PjfL_OUgrk_MJ64JG.bLDXkx_XazODfr4-'
+require 'going_postal'
+require 'geocoder'
+module Weather
+	appid = 'LCQO6F3V34FE6FIki6XMq3Pg8pd46EgovEi7AYFFTcA4cStxIre2CB8tpE7Xy.PjfL_OUgrk_MJ64JG.bLDXkx_XazODfr4-'
+	@client = YahooWeather::Client.new(appid)
+	country = 'US'
 
-@client = YahooWeather::Client.new(appid)
+	def get_weather(zip)
 
-response = @client.lookup_zip(49931)
-pp "Info"
-pp response.title
-pp response.condition.temp
-pp response.condition.text
-pp "Conditions"
-pp response.condition.temp
-pp response.units.temperature
-pp response.condition.text
-pp "Forecast"
-pp "#{response.forecasts[0].day} - #{response.forecasts[0].text}.  High: #{response.forecasts[0].high} Low: #{response.forecasts[0].low}"
-pp "#{response.forecasts[1].day} - #{response.forecasts[1].text}.  High: #{response.forecasts[1].high} Low: #{response.forecasts[1].low}"
+		zip = GoingPostal.postcode? zip, country
+
+		begin
+
+		response = @client.lookup_zip(zip)
+
+		message = ""
+
+		@ForC = response.units.temperature
+		loc = response.title
+		index = loc.rindex(',')
+		index = index + 3
+		space = loc.index(' ') + 1
+
+		loc = loc[space..index]
+		message << "Weather #{loc}"
+		message << "\n"
+		message << "#{response.condition.temp}#{ForC} - #{response.condition.text}"
+		message << "\n"
+
+		dir = Geocoder::Calculations.compass_point(response.wind.direction)
+
+		message << "Wind #{dir} #{response.wind.speed}#{response.units.speed}. Feels like #{response.wind.chill}#{ForC}"
+		message << "\n"
+		message << "Tomorrow's Forecast"
+		message << "\n"
+		message << "#{response.forecasts[1].text}.  High: #{response.forecasts[1].high} Low: #{response.forecasts[1].low}"
+		message << "\n"
+
+		rescue
+		 	message = "ERROR 69:\nSomething went wrong."
+
+		end #begin
+
+		return message
+	end #def
+end #module
+# Sample Output
+	# Weather for Beverly Hills, CA
+	# 60F - Mostly Cloudy
+	# Wind 8mph at 250. Feels like 60F
+	# Tomorrow's Forecast
+	# AM Clouds/PM Sun. High: 66 Low: 52
