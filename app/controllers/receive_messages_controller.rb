69816876e3501836cf68b1b659a5655dc5721c76
Brandon Gafford <brandon.gafford@gmail.com>
2013-03-28 13:35:39 -0400

    fixes response view (extra >), fixes number lookup

--- app/controllers/receive_messages_controller.rb
+++ app/controllers/receive_messages_controller.rb
@@ -16,7 +16,7 @@ class ReceiveMessagesController < ApplicationController
 
     # find the user's name from their phone number
     fromNumRaw = params[:From].split("")
-    fromNum = "#{fromNumRaw[2..4].join}-#{fromNumRaw[5..7].join}-#{fromNumRaw[8..1].join}"
+    fromNum = "#{fromNumRaw[2..4].join}-#{fromNumRaw[5..7].join}-#{fromNumRaw[8..-1].join}"
     user= User.find_by_phone_number(fromNum)
     unless(user.nil?)
       @userName = user.name
