be0bc8c4e5a2ee155d2509ffa7644ee8b347488d
Brandon Gafford <brandon.gafford@gmail.com>
2013-03-28 17:47:10 -0400

    moved message sending code back into its own method

--- lib/twilio_helper.rb
+++ lib/twilio_helper.rb
@@ -5,31 +5,30 @@ module TwilioHelper
   ACCOUNT_SID = 'ACd596ace63991a2ee1c1d04d511bb929d'
   AUTH_TOKEN = 'fd6608ce6be77e3f0e4a839e60021353'
 
-  def send_text(group, message)
-    @text_message = TextMessage.new(group_name: group, message: message)
-    
-    successes = []
-    errors = []
+  def send_group_text(group, message)
+    text_message = TextMessage.new(group_name: group, message: message)
 
     if @text_message.valid?
       numbers = @text_message.numbers_array
       
       numbers.each do |number|
-        account = Twilio::REST::Client.new(ACCOUNT_SID, AUTH_TOKEN).account
-
-        logger.info "sending message: #{message} to: #{number}"
-        begin
-          account.sms.messages.create(
-              :from => TWILIO_NUMBER,
-              :to => "+1#{number}",
-              :body => @text_message.message
-          )
-          successes << "#{number}"
-        rescue Exception => e
-          logger.error "error sending message: #{e.to_s}"
-          errors << "#{e.to_s}"
-        end # begin
+        send_text(number, text_message.number)
       end # num each
     end # if
   end # def
+
+  def send_text(number, message)
+    account = Twilio::REST::Client.new(ACCOUNT_SID, AUTH_TOKEN).account
+
+    logger.info "sending message: #{message} to: #{number}"
+    begin
+      account.sms.messages.create(
+          :from => TWILIO_NUMBER,
+          :to => "+1#{number}",
+          :body => message
+      )
+    rescue Exception => e
+      logger.error "error sending message: #{e.to_s}"
+    end # begin
+  end
 end # module
