9e1847457f3e997f1a48dc3babd29442e072cbce
Brandon Gafford <brandon.gafford@gmail.com>
2013-03-28 13:46:26 -0400

    fixes parser

--- lib/parse_helper.rb
+++ lib/parse_helper.rb
@@ -57,7 +57,7 @@ module ParseHelper
 
 	def messageGroup(from, group, message)
 		# send the message
-    	@message = "#{@group}: #{@userName} - #{@message}"
+    	@message = "#{group}: #{from} - #{message}"
     	send_text(@group, @message)
 	end
 end
