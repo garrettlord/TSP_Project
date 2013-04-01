0a56820f09f329cb0e5b95ee60ee12a5a5ef1f4d
Garrett Lord <garrettlord@gmail.com>
2013-04-01 16:55:42 -0400

    working wordnik

--- api_scripts/wordnik_example.rb
+++ api_scripts/wordnik_example.rb
@@ -9,9 +9,23 @@ Wordnik.configure do |config|
     config.api_key = 'd41d2d7c76970666c300a0a3cfa019bd4fd633ddc240fadb0'
 end
 
-	word = Wordnik.word.get_definitions('Team',
-                                     partOfSpeech='verb',
-                                     sourceDictionaries='wiktionary',
-                                     limit=1)
+	word = Wordnik.word.get_definitions('live', :limit=>2, :source_dictionaries => 'webster')
+	example = Wordnik.word.get_examples('school')
+	related = Wordnik.word.get_related('sad', :type => 'synonym')
+	pron = Wordnik.word.get_text_pronunciations('apple')
+	#scrabble = Wordnik.word.get_scrabble_score('Score')
+	wotd = Wordnik.words.get_word_of_the_day()
+	random = Wordnik.words.get_random_word(:part_of_speech => 'verb', :part_of_speech => 'noun', :part_of_speech => 'adjective', :minLength => '3', :maxLength => '4')
+
 
 puts word[0]["text"]
+puts "---------------------"
+puts example["examples"][0]["text"]
+puts "---------------------"
+puts related[0]["words"]
+puts "------------------------"
+puts wotd["word"]
+puts wotd["examples"][0]["text"]
+puts wotd["examples"][1]["text"]
+puts "------------------------"
+puts random["word"]
