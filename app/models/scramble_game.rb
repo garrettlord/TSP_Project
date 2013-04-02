122fb1b9ced38796fbbacb3ac8d7bf56971e1612
Matthaeus Saavedra <masaaved@mtu.edu>
2013-04-01 23:21:58 -0400

    scrambleGame without randomized words

--- app/models/scramble_game.rb
+++ app/models/scramble_game.rb
@@ -3,4 +3,18 @@ class ScrambleGame < ActiveRecord::Base
 
   belongs_to :user
   has_one :scramble_round
+
+  def initScrambleGame(user_id)
+  	@user = User.find(user_id)
+  	@score = 0
+  	@scramble_round = Round.new
+  end
+
+  def startRound(word)
+  	out = @sramble_round.checkWord(word)
+  	if @scramble_round.isOver
+  		out << " Your total score is: #{@score}"
+  	end
+  	return out
+  end
 end
--- app/models/scramble_round.rb
+++ app/models/scramble_round.rb
@@ -1,5 +1,57 @@
 class ScrambleRound < ActiveRecord::Base
-  attr_accessible :correctWord, :roundScore, :scramble_game_id, :scrambledWord, :usedHint
+  attr_accessible :correctWord, :roundScore, :scramble_game_id, :scrambledWord, :usedHint, :finishedRound
 
   belongs_to :scramble_game
+
+  def initRound
+  	@roundScore = 10
+  	@usedHint = 0
+  	@finishedRound = 0
+  	#Generate random word from Garret's code
+
+  	return "Starting new round... Scrambled word is: #{scrambledWord}"
+
+  end
+
+  def checkWord(word)
+  	if finishedRound == 1
+  		initRound
+  	end
+
+  	if word[0, 1] == "h "
+  		if @usedHint == 0
+  			hint(word)
+  			@roundScore += -4
+  			return @usedHint = 1
+  		else
+  			return "You have already used your hint for this round"
+  		end
+  		return hint(word)
+  	else
+  		if word == correctWord
+  			@finishedRound = 1
+  			return "Correct! Score for this round is: #{@roundScore}."
+  		else
+  			@roundScore += -2
+  			if @roundScore <= 0
+  				@finishedRound = 1
+  				return "Round over! You used all your attempts"
+  			end
+  			return "Incorrect! Please try again."
+  		end
+  	end
+  	@roundScore
+  end
+
+  def hint(word)
+  	#Call to Garret's thing, get definition
+  end
+
+  def randomizeString(word)
+  	word.split("").shuffle.join
+  end
+
+  def isOver
+  	@finishedRound
+  end
 end
