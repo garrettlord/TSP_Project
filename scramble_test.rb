ec8da723c012371cba98a98f9dcf8c1b06537c7a
Matthaeus Saavedra <masaaved@mtu.edu>
2013-04-08 15:38:40 -0400

    update of my code and test file

--- scramble_test.rb
+++ scramble_test.rb
@@ -1,5 +1,7 @@
 require File.join(Rails.root, "lib/scramble_helper.rb")
 
-testUser = User.new(:name => "Derp", :phone_number => "1234567890", :password => "password", :password_confirmation => "password")
+begin
+	testUser = User.new(:name => "Derp", :phone_number => "1234567890", :password => "password", :password_confirmation => "password")
 
-playGame(testUser, "derp")
+	playGame(testUser, "derp")
+end
