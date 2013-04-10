4554751c4bb4b4753e7e0f77d9f83792bfc93630
Garrett Lord <garrettlord@gmail.com>
2013-04-09 22:52:00 -0400

    Script is working + basic graphing

--- api_scripts/reporting.rb
+++ api_scripts/reporting.rb
@@ -10,6 +10,46 @@ AUTH_TOKEN = 'fd6608ce6be77e3f0e4a839e60021353'
 
 @client = Twilio::REST::Client.new(ACCOUNT_SID, AUTH_TOKEN)
 
+#A daily count over a date range of all inbound texts
+
+@inbound = []
+@allTexts = []
+
+puts "just inbound texts"
+@client.account.usage.records.daily.list({
+   :category => "sms-inbound",
+   :start_date => "2013-04-02",
+   :end_date => "2013-04-06"
+  }).each do |record|
+    # puts record.start_date
+    # puts record.count
+    # if record.count.nil? 
+    #   @inbound << 0
+    # else
+      @inbound << record.count
+    # end
+end
+
+puts @inbound
+
+puts "---------------------------------------"
+#A daily count over a date range of all inbound and outbound text
+
+puts "inbound and outbound texts"
+@client.account.usage.records.daily.list({
+   :category => "sms",
+   :start_date => "2013-04-02",
+   :end_date => "2013-04-06"
+  }).each do |record|
+  # puts record.start_date
+  @allTexts << record.count
+  # puts record.count
+end
+
+puts @allTexts
+
+
+
 # puts @client.account.methods.sort
  
  
@@ -21,30 +61,9 @@ AUTH_TOKEN = 'fd6608ce6be77e3f0e4a839e60021353'
 
 # Loop over messages and print out a property for each one
 # @account.sms.messages.list.each do |message|
-# 	puts "\ n"
+#   puts "\ n"
 #     puts message.body
 # end
-
-#A daily count over a date range of all inbound texts
-@client.account.usage.records.daily.list({
-   :category => "sms-inbound",
-   :start_date => "2013-04-02",
-   :end_date => "2013-04-06"
-  }).each do |record|
-    puts "just inbound texts"
-    puts record.count
-end
-
-#A daily count over a date range of all inbound and outbound text
-@client.account.usage.records.daily.list({
-   :category => "sms",
-   :start_date => "2013-04-02",
-   :end_date => "2013-04-06"
-  }).each do |record|
-  puts "inbound and outbound texts"
-    record.count
-end
-
 
 
 # @client.account.usage.records.last_month.list.each do |record|
