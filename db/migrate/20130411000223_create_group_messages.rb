a65843712be626b6ba097b4722fc8c60f5dfa830
Brandon Gafford <brandon.gafford@gmail.com>
2013-04-11 15:00:01 -0400

    removed defaults, fixed migration file

--- db/migrate/20130411000223_create_group_messages.rb
+++ db/migrate/20130411000223_create_group_messages.rb
@@ -2,7 +2,7 @@ class CreateGroupMessages < ActiveRecord::Migration
   def change
     create_table :group_messages do |t|
       t.integer :user_id
-      t.integer, :group_id
+      t.integer :group_id
       t.string :message
 
       t.timestamps
