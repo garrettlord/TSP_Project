44b63a4cacf08abab8a3dba3820f87762faea4f3
Brandon Gafford <brandon.gafford@gmail.com>
2013-04-10 23:17:20 -0400

    changes associations to default to non-admin

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -13,7 +13,7 @@ class GroupUsersController < ApplicationController
 
   def create
     @group_user = GroupUser.new(group_id: Group.find_by_name(params[:group_name]).id,
-        user_id: User.find_by_name(params[:user_name]).id)
+        user_id: User.find_by_name(params[:user_name]).id, admin: false)
     if @group_user.save
       flash[:success] = "New association created!"
       redirect_to @group_user
