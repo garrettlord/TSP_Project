3aad0b980c02dca2ffa3f2bd47808588d489e0c9
Brandon Gafford <brandon.gafford@gmail.com>
2013-04-12 11:49:03 -0400

    fixes bug in group message searchign

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,7 @@ class UsersController < ApplicationController
     @groups = Group.find(:all, conditions: {public: true})
     @groups_in = @user.all_groups
     @users = User.all
-    messages = GroupMessage.all
-    messages = GroupMessage.where("group_id = ? OR user_id = ?", @groups_in, @user.id)
+    messages = GroupMessage.where("group_id in (?) OR user_id = ?", @groups_in, @user.id)
     @message = ""
     messages.each do |msg|
       @message << "#{msg.message}\n"
