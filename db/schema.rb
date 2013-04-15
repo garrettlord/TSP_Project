eac7741a75cf2decf9e57e626c63d79fea75a049
Garrett Lord <garrettlord@gmail.com>
2013-04-15 16:40:54 -0400

    added the barchart stuff to api_script

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20130412155524) do
 
-  create_table "group_admins", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "group_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "group_messages", :force => true do |t|
     t.integer  "user_id"
     t.integer  "group_id"
